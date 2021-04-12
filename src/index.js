e4b95a895b916b354fe27b9acf9f5f0c752ace51
haydenbp <32523980+haydenbp@users.noreply.github.com>
2021-04-11 21:24:25 -0400

    s

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import _ from 'lodash';
+
+import myName from './myName';
+
 
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+
+    // use your function!
+    element.innerHTML = myName('Cody');
     return element;
-  }
-  
-  document.body.appendChild(component());
+}
+
+document.body.appendChild(component());
